cf720bff883b4b41d5967850d1f5346330b2c817
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-22 14:39:36 -0300

    :sparkles: feat: Create SQL consult's

--- src/main/java/com/papelariafrasato/api/repositories/OrderItemRepository.java
+++ src/main/java/com/papelariafrasato/api/repositories/OrderItemRepository.java
@@ -2,8 +2,15 @@ package com.papelariafrasato.api.repositories;
 
 import com.papelariafrasato.api.models.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, String> {
+
+    @Query(value = "SELECT * FROM order_item WHERE order_id = :order_id", nativeQuery = true)
+    List<OrderItem> findOrderItemByIdOrderId(@Param("order_id")String orderId);
 }
--- src/main/java/com/papelariafrasato/api/repositories/OrderRepository.java
+++ src/main/java/com/papelariafrasato/api/repositories/OrderRepository.java
@@ -12,5 +12,11 @@ import java.util.Optional;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
     @Query(value = "SELECT * FROM orders WHERE orders.status = :status", nativeQuery = true)
-    List<Optional<Order>> findByAStatus(@Param("status") String status);
+    List<Order> findByAStatus(@Param("status") String status);
+
+    @Query(value = "SELECT * FROM orders WHERE orders.user_id = :user_id", nativeQuery = true)
+    List<Order> findByUserId(@Param("user_id") String user_id);
+
+    @Query(value = "SELECT * FROM orders WHERE orders.user_id = :user_id AND orders.status = 'open'", nativeQuery = true)
+    Optional<Order> findOrderByUserId(@Param("user_id")String user_id);
 }

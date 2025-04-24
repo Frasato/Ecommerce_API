7aad74cec5ce9df567aacdce08df59ddb762f1ce
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-24 12:11:02 -0300

    :sparkles: feat: Function to create and add Discount on Product

--- src/main/java/com/papelariafrasato/api/services/ProductService.java
+++ src/main/java/com/papelariafrasato/api/services/ProductService.java
@@ -4,7 +4,6 @@ import com.papelariafrasato.api.models.Product;
 import com.papelariafrasato.api.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -16,5 +15,32 @@ public class ProductService {
 
     public List<Product> getAllProducts(){
         return productRepository.findAll();
+    }
+
+    public void createProduct(String productName, String productDescription, double price, String category){
+        Product product = new Product();
+
+        product.setProduct_name(productName);
+        product.setProduct_description(productDescription);
+        product.setPrice(price);
+        product.setCategory(category);
+        product.setDiscount(0);
+
+        productRepository.save(product);
+    }
+
+    public void addDiscountOnProduct(String productId, double discount){
+        try{
+            Optional<Product> findedProduct = productRepository.findById(productId);
+
+            if(findedProduct.isPresent()){
+                Product product = findedProduct.get();
+                product.setDiscount(discount);
+                productRepository.save(product);
+            }
+        }catch(Exception exception){
+            throw new RuntimeException("ERROR: " + exception.getMessage());
+        }
+
     }
 }

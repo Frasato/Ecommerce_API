13c2f1d6da1021873fa78ba704acfa6d2f5e8881
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-15 13:40:35 -0300

    :sparkles: feat: Create cart controller

---
+++ src/main/java/com/papelariafrasato/api/controllers/CartController.java
@@ -0,0 +1,36 @@
+package com.papelariafrasato.api.controllers;
+
+import com.papelariafrasato.api.dtos.AddItemToCartRequestDto;
+import com.papelariafrasato.api.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCart(@PathVariable("id")String userId){
+        return cartService.findAllCartItemsUser(userId);
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> addItem(@RequestBody AddItemToCartRequestDto itemDto){
+        return cartService.addItemOnCart(itemDto.userId(), itemDto.productId());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> removeItemFromCart(@PathVariable("id")String cartItemId){
+        return cartService.removeItemFromCart(cartItemId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> clearCart(@PathVariable("id")String userId){
+        return cartService.clearCart(userId);
+    }
+
+}

e5a914efa3ae0eb9ba1c828a40fb230c909fc740
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-15 13:47:07 -0300

    :sparkles: feat: Query to find address by user

--- src/main/java/com/papelariafrasato/api/repositories/AddressRepository.java
+++ src/main/java/com/papelariafrasato/api/repositories/AddressRepository.java
@@ -2,7 +2,14 @@ package com.papelariafrasato.api.repositories;
 
 import com.papelariafrasato.api.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AddressRepository extends JpaRepository<Address, String>{}
+public interface AddressRepository extends JpaRepository<Address, String>{
+    @Query(value = "SELECT * FROM address WHERE user_id = :userId", nativeQuery = true)
+    Optional<Address> findByUserId(@Param("userId")String userId);
+}

7394f568997f5292234a6a3f8592efdc63bd8ef1
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-15 13:46:19 -0300

    :sparkles: feat: Create OrderItem Entity

---
+++ src/main/java/com/papelariafrasato/api/models/OrderItem.java
@@ -0,0 +1,27 @@
+package com.papelariafrasato.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}

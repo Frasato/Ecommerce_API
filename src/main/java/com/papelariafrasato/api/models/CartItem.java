f2c6c6a6aa7ed1cfe5056b064dcaaf1ad97ba505
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-15 13:45:55 -0300

    :sparkles: feat: Create CartItem Entity

---
+++ src/main/java/com/papelariafrasato/api/models/CartItem.java
@@ -0,0 +1,27 @@
+package com.papelariafrasato.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private Integer quantity;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}

e0deb2874316c26651848f444b8fea9fa3742f1a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-19 16:11:31 -0300

    :sparkles: feat: Create a payment entity

---
+++ src/main/java/com/papelariafrasato/api/models/Payment.java
@@ -0,0 +1,24 @@
+package com.papelariafrasato.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "info_payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String paymentId;
+    @ManyToOne
+    private User user;
+
+}

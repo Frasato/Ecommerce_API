a313dbad708f8bfe08bdce0f7407a98e30a85111
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-21 15:40:51 -0300

    :sparkles: feat: Call my API to save images

--- src/main/java/com/papelariafrasato/api/services/ProductService.java
+++ src/main/java/com/papelariafrasato/api/services/ProductService.java
@@ -1,32 +1,64 @@
 package com.papelariafrasato.api.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.papelariafrasato.api.exceptions.InvalidPriceException;
 import com.papelariafrasato.api.exceptions.ProductNotFoundException;
 import com.papelariafrasato.api.models.Product;
 import com.papelariafrasato.api.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public ResponseEntity<?> allProducts() {
         return ResponseEntity.ok().body(productRepository.findAll());
     }
 
     @Transactional
-    public ResponseEntity<?> addProduct(String image, String name, String description, Integer price, String category) {
+    public ResponseEntity<?> addProduct(MultipartFile image, String name, String description, Integer price, String category) throws IOException {
         if (price < 0) {
             throw new InvalidPriceException();
         }
 
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new org.springframework.core.io.ByteArrayResource(image.getBytes()){
+            @Override
+            public String getFilename(){
+                return image.getOriginalFilename();
+            }
+        });
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        String response = restTemplate.postForObject("http://your-image/api/here", requestEntity, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response);
+        String urlImage = root.get("url").asText();
+
         Product product = new Product();
-        product.setImage(image);
+        product.setImage(urlImage);
         product.setName(name);
         product.setDescription(description);
         product.setPrice(price);

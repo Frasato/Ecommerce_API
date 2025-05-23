afef247a29389a01411022afd529da6644430928
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-23 12:35:07 -0300

    :sparkles: feat: Query to select products by category

--- src/main/java/com/papelariafrasato/api/repositories/ProductRepository.java
+++ src/main/java/com/papelariafrasato/api/repositories/ProductRepository.java
@@ -2,7 +2,14 @@ package com.papelariafrasato.api.repositories;
 
 import com.papelariafrasato.api.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, String>{}
+public interface ProductRepository extends JpaRepository<Product, String>{
+    @Query(value = "SELECT * FROM products WHERE category = :category", nativeQuery = true)
+    List<Product> getProductsByCategory(@Param("category")String category);
+}

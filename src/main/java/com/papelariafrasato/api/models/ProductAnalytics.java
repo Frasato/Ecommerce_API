8df0537481b1898e292bd9b6dc05f7dabe1e94e7
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-29 11:35:25 -0300

    :sparkles: feat: Create Entity For analytics

---
+++ src/main/java/com/papelariafrasato/api/models/ProductAnalytics.java
@@ -0,0 +1,38 @@
+package com.papelariafrasato.api.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "products_analytics")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductAnalytics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private Integer purchase;
+    private Integer click;
+    private Integer cartAdded;
+    private Instant dateTime;
+    @JsonManagedReference
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @PrePersist
+    private void setTime(){
+        this.dateTime = Instant.now();
+    }
+
+}

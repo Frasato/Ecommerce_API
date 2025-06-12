ff2ee7beb847e0f37e3353107838aac4b8ff525d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-12 16:37:09 -0300

    :sparkles: feat: Add right exception on returns

--- src/main/java/com/papelariafrasato/api/services/UserService.java
+++ src/main/java/com/papelariafrasato/api/services/UserService.java
@@ -1,5 +1,7 @@
 package com.papelariafrasato.api.services;
 
+import com.papelariafrasato.api.exceptions.EmptyInformationException;
+import com.papelariafrasato.api.exceptions.UserNotFoundException;
 import com.papelariafrasato.api.models.User;
 import com.papelariafrasato.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +21,14 @@ public class UserService {
         try{
             Optional<User> foundedUser = userRepository.findById(userId);
 
-            if (userId.isEmpty()) return ResponseEntity.badRequest().body("User ID can't be empty!");
+            if (userId.isEmpty()) throw new EmptyInformationException();
 
             if (foundedUser.isPresent()) {
                 User user = foundedUser.get();
                 return ResponseEntity.ok().body(user);
             }
 
-            return ResponseEntity.notFound().build();
+            throw new UserNotFoundException(userId);
         }catch(Exception e){
             return ResponseEntity.internalServerError().body(e.getMessage());
         }

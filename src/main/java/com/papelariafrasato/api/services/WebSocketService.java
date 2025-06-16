f158f0c116d949c08f6d24bdaaa37c5c93ab9add
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-16 12:47:41 -0300

    :sparkles: feat: Save messages and get all them for history chat

--- src/main/java/com/papelariafrasato/api/services/WebSocketService.java
+++ src/main/java/com/papelariafrasato/api/services/WebSocketService.java
@@ -1,7 +1,10 @@
 package com.papelariafrasato.api.services;
 
+import com.papelariafrasato.api.dtos.ChatDto;
 import com.papelariafrasato.api.models.Chat;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -12,11 +15,13 @@ import java.util.stream.Collectors;
 public class WebSocketService {
 
     private final Map<String, Chat> activeChats = new ConcurrentHashMap<>();
+    private final Map<String, List<ChatDto>> chatMessages = new ConcurrentHashMap<>();
 
     public String createChat(String userId) {
         String chatId = UUID.randomUUID().toString();
         Chat chat = new Chat(chatId, userId, true);
         activeChats.put(chatId, chat);
+        chatMessages.put(chatId, new ArrayList<>());
         return chatId;
     }
 
@@ -31,5 +36,13 @@ public class WebSocketService {
         return activeChats.values().stream()
                 .filter(Chat::isActive)
                 .collect(Collectors.toList());
+    }
+
+    public void saveMessage(ChatDto chatDto){
+        chatMessages.computeIfAbsent(chatDto.chatId(), k -> new ArrayList<>()).add(chatDto);
+    }
+
+    public List<ChatDto> getChatHistory(String chatId){
+        return chatMessages.getOrDefault(chatId, new ArrayList<>());
     }
 }

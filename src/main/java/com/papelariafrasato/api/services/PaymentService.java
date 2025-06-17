5a40226a6f52a99fe173d9a26f43c6f9f5e942ed
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-17 12:11:43 -0300

    :bug: fix: Autowired annotation missing

--- src/main/java/com/papelariafrasato/api/services/PaymentService.java
+++ src/main/java/com/papelariafrasato/api/services/PaymentService.java
@@ -4,13 +4,16 @@ import com.papelariafrasato.api.dtos.RequestPaymentCardDto;
 import com.papelariafrasato.api.dtos.ResponsePaymentCardDto;
 import com.papelariafrasato.api.utils.BuildXml;
 import com.papelariafrasato.api.utils.PaymentHttp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
+    @Autowired
     private BuildXml buildXml;
+    @Autowired
     private PaymentHttp paymentHttp;
 
     public ResponseEntity<?> processPayment(RequestPaymentCardDto paymentCardDto){
@@ -36,7 +39,7 @@ public class PaymentService {
             return ResponseEntity.ok(response);
 
         }catch(Exception e){
-            throw new RuntimeException("Error on process the payment: " + e.getMessage());
+            return ResponseEntity.internalServerError().body("Error on process the payment: " + e.getMessage());
         }
     }
 

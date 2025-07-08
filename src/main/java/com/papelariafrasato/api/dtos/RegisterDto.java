b0d451944424920c8ee88c43fef526b9db41c7a3
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-07-08 09:25:08 -0300

    :broom: cleanup: Remove unnecessary parameters

--- src/main/java/com/papelariafrasato/api/dtos/RegisterDto.java
+++ src/main/java/com/papelariafrasato/api/dtos/RegisterDto.java
@@ -12,17 +12,5 @@ public record RegisterDto(
         @Schema(description = "Phone number", example = "+1 55 99825771")
         String phone,
         @Schema(description = "User CPF", example = "11111111111")
-        String cpf,
-        @Schema(description = "Street name", example = "Av. Steven Smith")
-        String street,
-        @Schema(description = "House number", example = "32")
-        String number,
-        @Schema(description = "Country you live", example = "Washington... São Paulo...")
-        String countryState,
-        @Schema(description = "The district your house stay", example = "Crawford St... Av. 9 de julho")
-        String district,
-        @Schema(description = "City name", example = "Buenos Aires")
-        String city,
-        @Schema(description = "CEP number of the city", example = "111111111")
-        String CEP) {
-}
+        String cpf)
+{}

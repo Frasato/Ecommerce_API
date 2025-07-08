b6cf55e1a1d2a33eb152b080d6ae1b814f09ad3f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-07-08 10:00:17 -0300

    :books: docs: Better documentation for cart endpoint

--- src/main/java/com/papelariafrasato/api/controllers/CartController.java
+++ src/main/java/com/papelariafrasato/api/controllers/CartController.java
@@ -1,6 +1,8 @@
 package com.papelariafrasato.api.controllers;
 
 import com.papelariafrasato.api.dtos.AddItemToCartRequestDto;
+import com.papelariafrasato.api.dtos.ResponseUserDto;
+import com.papelariafrasato.api.exceptions.*;
 import com.papelariafrasato.api.models.Cart;
 import com.papelariafrasato.api.services.CartService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -30,8 +32,18 @@ public class CartController {
             description = "Get items on cart by user"
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Found cart", content = @Content(schema = @Schema(implementation = Cart.class))),
-            @ApiResponse(responseCode = "400", description = "Invalid information our empty information")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found and return the cart",
+                    content = @Content(schema = @Schema(implementation = Cart.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cart not found",
+                    content = @Content(schema = @Schema(implementation = CartNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "A internal server error occurred",
+                    content = @Content(schema = @Schema(implementation = InternalServerException.class))),
     })
     public ResponseEntity<?> getCart(@PathVariable("id")String userId){
         return cartService.findAllCartItemsUser(userId);
@@ -43,8 +55,28 @@ public class CartController {
             description = "Add items on cart"
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "Added item on cart"),
-            @ApiResponse(responseCode = "400", description = "Invalid information our empty information")
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Cart created and add item to the cart"),
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Added item to the cart"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User not found",
+                    content = @Content(schema = @Schema(implementation = UserNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cart not found",
+                    content = @Content(schema = @Schema(implementation = CartNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product not found",
+                    content = @Content(schema = @Schema(implementation = ProductNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "A internal server error occurred",
+                    content = @Content(schema = @Schema(implementation = InternalServerException.class))),
     })
     public ResponseEntity<?> addItem(@RequestBody AddItemToCartRequestDto itemDto){
         return cartService.addItemOnCart(itemDto.userId(), itemDto.productId());
@@ -56,8 +88,17 @@ public class CartController {
             description = "Remove item on cart"
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Item was removed"),
-            @ApiResponse(responseCode = "400", description = "Invalid information our empty information")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Item removed of the cart"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cart Item not found",
+                    content = @Content(schema = @Schema(implementation = CartItemNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "A internal server error occurred",
+                    content = @Content(schema = @Schema(implementation = InternalServerException.class))),
     })
     public ResponseEntity<?> removeItemFromCart(@PathVariable("id")String cartItemId){
         return cartService.removeItemFromCart(cartItemId);
@@ -69,8 +110,17 @@ public class CartController {
             description = "Clear the cart, remove all items on the cart"
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Cleaned"),
-            @ApiResponse(responseCode = "400", description = "Invalid information our empty information")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The cart is cleaned"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cart not found",
+                    content = @Content(schema = @Schema(implementation = CartNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "A internal server error occurred",
+                    content = @Content(schema = @Schema(implementation = InternalServerException.class))),
     })
     public ResponseEntity<?> clearCart(@PathVariable("id")String userId){
         return cartService.clearCart(userId);
@@ -82,8 +132,17 @@ public class CartController {
             description = "Get the cart item and plus one in the quantity of him"
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Plus one success"),
-            @ApiResponse(responseCode = "400", description = "Invalid information our empty information")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Add 1 of the amount of the cart item"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cart Item not found",
+                    content = @Content(schema = @Schema(implementation = CartItemNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "A internal server error occurred",
+                    content = @Content(schema = @Schema(implementation = InternalServerException.class))),
     })
     public ResponseEntity<?> plusOneCartItem(@PathVariable("cartItemId")String cartItemId){
         return cartService.plusOneCartItem(cartItemId);
@@ -95,8 +154,17 @@ public class CartController {
             description = "Get the cart item and minus one, if the cart item has just one item, he will be deleted"
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Minus one success"),
-            @ApiResponse(responseCode = "400", description = "Invalid information our empty information")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Remove 1 of the amount of the cart item"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cart Item not found",
+                    content = @Content(schema = @Schema(implementation = CartItemNotFoundException.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "A internal server error occurred",
+                    content = @Content(schema = @Schema(implementation = InternalServerException.class))),
     })
     public ResponseEntity<?> minusOneCartItem(@PathVariable("cartItemId")String cartItemId){
         return cartService.minusOneCartItem(cartItemId);

687021a8bd73c0c52c4fe7f026b245477342c2d3
Frasato <gabriel.frasato@hotmail.com>
2025-12-02 16:29:25 -0300

    feat: Create a model for save payments

---
+++ src/main/java/com/papelariafrasato/api/models/Payment.java
@@ -0,0 +1,30 @@
+package com.papelariafrasato.api.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String paymentId;
+    private String paymentType;
+    private String status;
+
+    @JsonBackReference
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}

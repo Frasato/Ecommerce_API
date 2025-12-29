17064e0c459e4028d6a3469dccff18b33c7c3456
Frasato <gabriel.frasato@hotmail.com>
2025-12-29 12:22:29 -0300

    feat: create queries for find products by discount and expired discount

--- src/main/java/com/papelariafrasato/api/repositories/ProductRepository.java
+++ src/main/java/com/papelariafrasato/api/repositories/ProductRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String>{
     @Query(value = "SELECT * FROM products WHERE category = :category", nativeQuery = true)
     List<Product> getProductsByCategory(@Param("category")String category);
+
+    @Query(value = "SELECT * FROM products WHERE discountEnd IS NOT NULL", nativeQuery = true)
+    List<Product> getProductsByDiscount();
+
+    @Query(value = "SELECT * FROM products WHERE discountEnd IS NOT NULL AND discountEnd <= :currentDate", nativeQuery = true)
+    List<Product> findExpiredDiscounts(@Param("currentDate") LocalDateTime currentDate);
 }

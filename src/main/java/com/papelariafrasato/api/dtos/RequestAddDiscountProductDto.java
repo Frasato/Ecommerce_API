4cabafe4c28569db0c1f2f87b2b8a9b5bcf5e87b
Frasato <gabriel.frasato@hotmail.com>
2025-12-29 12:23:27 -0300

    feat: add the discount end date on request dto

--- src/main/java/com/papelariafrasato/api/dtos/RequestAddDiscountProductDto.java
+++ src/main/java/com/papelariafrasato/api/dtos/RequestAddDiscountProductDto.java
@@ -1,10 +1,13 @@
 package com.papelariafrasato.api.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
 
 public record RequestAddDiscountProductDto(
-        @Schema(description = "UUID user id", example = "adaw331-asdaf31-242fr2-qd31fe33")
+        @Schema(description = "product ID", example = "adaw331-asdaf31-242fr2-qd31fe33")
         String productId,
-        @Schema(description = "UUID product id", example = "adaw331-ffke59-242fr2-zxxl990")
-        int discount
+        @Schema(description = "Discount on percent(%)", example = "50%")
+        int discount,
+        @Schema(description = "The end of the product discount", example = "12/03")
+        LocalDate discountEnd
 ){}

7ae53c80336801d60dac91713bb4e02c9c86897a
Frasato <gabriel.frasato@hotmail.com>
2025-12-29 12:24:10 -0300

    chore: configure a service to remove discount on products by date

---
+++ src/main/java/com/papelariafrasato/api/services/DiscountScheduleService.java
@@ -0,0 +1,32 @@
+package com.papelariafrasato.api.services;
+
+import com.papelariafrasato.api.models.Product;
+import com.papelariafrasato.api.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class DiscountScheduleService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void removeExpiredDiscounts() {
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Product> expiredProducts = productRepository.findExpiredDiscounts(now);
+
+        expiredProducts.forEach(product -> {
+            product.setDiscount(0);
+            product.setPriceWithDiscount(0);
+            product.setDiscountEnd(null);
+            product.setDiscountInit(null);
+        });
+
+        productRepository.saveAll(expiredProducts);
+    }
+}

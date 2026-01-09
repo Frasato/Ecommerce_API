792e05deef547f2d26cbc2adaaa2456c65fd7998
Frasato <gabriel.frasato@hotmail.com>
2026-01-09 10:28:02 -0300

    fix (query): fixing the query for select products with discount

--- src/main/java/com/papelariafrasato/api/repositories/ProductRepository.java
+++ src/main/java/com/papelariafrasato/api/repositories/ProductRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,9 +13,14 @@ public interface ProductRepository extends JpaRepository<Product, String>{
     @Query(value = "SELECT * FROM products WHERE category = :category", nativeQuery = true)
     List<Product> getProductsByCategory(@Param("category")String category);
 
-    @Query(value = "SELECT * FROM products WHERE discountEnd IS NOT NULL", nativeQuery = true)
+    @Query(value = """
+      SELECT * FROM products 
+      WHERE discount IS NOT NULL 
+        AND discount > 0
+        AND discount_end >= CURRENT_DATE
+      """, nativeQuery = true)
     List<Product> getProductsByDiscount();
 
-    @Query(value = "SELECT * FROM products WHERE discountEnd IS NOT NULL AND discountEnd <= :currentDate", nativeQuery = true)
+    @Query(value = "SELECT * FROM products WHERE discount_end IS NOT NULL AND discount_end <= :currentDate", nativeQuery = true)
     List<Product> findExpiredDiscounts(@Param("currentDate") LocalDateTime currentDate);
 }
